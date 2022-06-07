9685e5334279803f74625e874f2e534c1bb36ff4
Ryww0 <laurent.chaitas@gmail.com>
2022-06-07 10:46:07 +0200

    bug

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener("clickgit", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
