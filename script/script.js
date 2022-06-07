fdda1811077d982da5835112a5188fbecf5ccbcc
Ryww0 <laurent.chaitas@gmail.com>
2022-06-07 11:20:56 +0200

    merge nav + header

--- script/script.js
+++ script/script.js
@@ -1,13 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("clickgit", mobileMenu);
+hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
-
 const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
