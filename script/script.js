80e49450126b506af9e03c3b2fc276d8c630cc07
Ryww0 <laurent.chaitas@gmail.com>
2022-06-09 09:50:10 +0200

    prettier

--- script/script.js
+++ script/script.js
@@ -4,55 +4,49 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
 
 // horaires
 
 function openHoraires() {
-    document.getElementById('horaires').style.display = "block";
+  document.getElementById("horaires").style.display = "block";
 }
 
 function closeHoraires() {
-    document.getElementById('horaires').style.display = "none";
+  document.getElementById("horaires").style.display = "none";
 }
-
 
 // anime scroll
 // if(window.innerWidth>768) {
-    const slidingAnime = document.querySelector('.slideIn')
+const slidingAnime = document.querySelector(".slideIn");
 
-    window.addEventListener('scroll', () => {
-   
-    const {scrollTop, clientHeight} =
-    document.documentElement;
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
 
-    const topElemetToTopViewport =
-     slidingAnime.getBoundingClientRect().top;
+  const topElemetToTopViewport = slidingAnime.getBoundingClientRect().top;
 
-
-     if(scrollTop > (scrollTop + topElemetToTopViewport).toFixed() -
-     clientHeight * 0.80) {
-         slidingAnime.classList.add('start')
-     }
-    })
+  if (
+    scrollTop >
+    (scrollTop + topElemetToTopViewport).toFixed() - clientHeight * 0.8
+  ) {
+    slidingAnime.classList.add("start");
+  }
+});
 // }
 
-
 // loader
-const loader = document.querySelector('.loader');
+const loader = document.querySelector(".loader");
 
-window.addEventListener('load', () => {
-
-    loader.classList.add('fondu-out');
-
-})
+window.addEventListener("load", () => {
+  loader.classList.add("fondu-out");
+});
